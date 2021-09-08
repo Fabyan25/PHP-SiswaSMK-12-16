d0dd3fda8eb11768226e82ba1337949e1f15a6d5
Fabyan25 <87288561+Fabyan25@users.noreply.github.com>
2021-09-08 19:17:00 +0700

    Add files via upload

---
+++ PHP-SiswaSMK-Fabyan-12-16/kontak.php
@@ -0,0 +1,2 @@
+<h1>Kontak Kami</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, magni.</p>
---
+++ PHP-SiswaSMK-Fabyan-12-16/sejarah.php
@@ -0,0 +1,2 @@
+<h1>Sejarah</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, iure?</p>
